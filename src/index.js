16313959e68a04a262df0396ed1731875665999e
gloriacullelldeveloper <gcullelldev@gmail.com>
2024-06-13 21:21:58 +0200

    Finished JS Code Adding Timezone with a function

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-
-  
-let cities = document.querySelector("#cities");
-cities.addEventListener('change', function () {
+ let cities = document.querySelector("#cities");
+  cities.addEventListener('change', function () {
 
     let citySelected = cities.value;
-   
+    let parisDate = moment().tz("Europe/Paris").format("dddd, MMMM D, YYYY LT");
+    let madridDate = moment().tz("Europe/Madrid").format("dddd, MMMM D, YYYY LT")
+    let berlinDate = moment().tz("Europe/Berlin").format("dddd, MMMM D, YYYY LT")
 
     switch (citySelected) {
-         case 'paris':
-            alert('It is currently ${parisDate} in Europe/Paris');
-             let parisDate = moment()
-          .tz("Europe/Paris")
-          .format("dddd, MMMM D, YYYY LT");
-      break;
-    case 'madrid':
-      alert('It is currently in Europe/Madrid');
-      break;
-    case 'berlin':
-      alert('It is currently in Europe/Berlin');
-      break;
-
+      case 'paris':
+        alert(`It is currently ${parisDate} in Europe/Paris`);
+        break;
+      case 'madrid':
+        alert(`It is currently ${madridDate} in Europe/Madrid`);
+        break;
+      case 'berlin':
+        alert(`It is currently ${berlinDate} in Europe/Berlin`);
+        break;
     }
+  });
 
-
-}); 
-    
+/*  let parisDate = moment()
+          .tz("Europe/Paris")
+          .format("dddd, MMMM D, YYYY LT"); */
